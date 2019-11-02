7212bcead37b300022a53b5b88626cbf7508a9c3
hisashi.sakurada <neofamilia39@icloud.com>
2019-11-02 08:33:03 +0000

    number_field

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   
   validates :menu, presence: true, length: { maximum:125 }
+  validates :weight, presence: true, :numericality => { :greater_than_or_equal_to => 0 } 
+  validates :counts, presence: true, :numericality => { :greater_than_or_equal_to => 1 } 
+  validates :set, presence: true, :numericality => { :greater_than_or_equal_to => 1 } 
 
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
